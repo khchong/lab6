89d042661704bd78aaa24065a46de76b179ec516
Kane Chong <khchong@ucsd.edu>
2015-02-11 20:01:55 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,21 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+
+	// MY CODE
+	var url = '/project/' + idNumber;
+	console.log(url);
+	$.get(url, function(result) {
+		console.log(result);
+
+		var htmlString = '<img src="' + result['image'] + '" class="detailsImage">' + 
+						 '<p><small>' + result['date'] + '</small></p>' +
+						 '<p>' + result['summary'] + '</p>';				
+
+		var projSelector = '.project[id=project' + idNumber + '] .details';
+		$(projSelector).html(htmlString);	
+	});
 }
 
 /*
@@ -35,4 +50,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	//e.preventDefault();
+	var url = '/palette';
+	$.get(url, function(result) {
+
+		console.log(result);
+		var colors = result.colors.hex;
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
+
 }
